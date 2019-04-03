425dd34da7fea6a0bf29948e6665e37c44c20d78
Ashley Nascimento <48355612+ashy-chan@users.noreply.github.com>
2019-04-02 21:01:24 -0300

    Back do cadastro de gerente e funcionario
    Foram feitos o backend da tela de cadastro de funcionário e cadastro de gerente.

---
+++ CONTROLLER/Transaction/CadastroGerenteTrans.php
@@ -0,0 +1,55 @@
+<?php
+
+//importa a conexão e a validação do cadastro
+
+include_once("../../MODEL/PHP files/DAOCadastroGeral.php");
+include_once("../Classes/Gerente.php");
+
+//instacia a conexao
+
+$cone = new DAOCadastroGeral();
+
+//pega as variaveis do html
+
+$nome = mysqli_real_escape_string($cone->cono, trim($_POST['nomeCompleto']));
+$email = mysqli_real_escape_string($cone->cono, trim($_POST['email']));
+$senha = "senhaGerente";
+$nivel = 2;
+$telefone = mysqli_real_escape_string($cone->cono, trim($_POST['telefone']));
+$dataNascimento = mysqli_real_escape_string($cone->cono, trim($_POST['dataNascimento']));
+$CPF = "";
+$endereco = "";
+$situacao = "";
+
+//instacia a classe
+
+$cadastroGerente = new Gerente();
+
+//envia os dados para classe;
+$cadastroGerente->Email = $email;
+$cadastroGerente->Password = $senha;
+$cadastroGerente->Niuser = $nivel;
+
+$cadastroGerente->nomeCompleto = $nome;
+$cadastroGerente->telefone = $telefone;
+$cadastroGerente->dataNascimento = $dataNascimento;
+$cadastroGerente->cpf = $CPF;
+$cadastroGerente->endereco = $endereco;
+$cadastroGerente->situacao = $situacao;
+
+//checa se o usuario existe
+
+$cone->Email = $cadastroGerente->Email; 
+
+if ($cone->checkUserName() == false){
+   $cone->classe = $cadastroGerente;
+   $cone->carregar();
+   $cone->createUser();
+}
+else{
+	echo "o usuário já existe!";
+}
+
+
+
+?>
