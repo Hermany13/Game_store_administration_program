264d5dccc04847adab178237bdc44c8d073070bf
Marlon Dantas <marlondantas2010@gmail.com>
2019-04-03 12:07:48 -0300

    re organizacao dos repositorios

--- CONTROLLER/Classes/Configuracao.php
+++ CONTROLLER/Classes/Configuracao.php
@@ -1,58 +1,3 @@
-<<<<<<< HEAD:CONTROLLER/Classes/Configuracao.php
-<?php
-
-require("Gerente.php");
-
-class Config{
-    private $nomeLoja;
-    private $logo;
-    private $endereco;
-    private $corPrincipal;
-    private $cnpj;
-    private $inscricaoEstadual;
-    private $taxaDesconto;
-
-    function Config(){
-        $this->nomeLoja = "";
-        $this->logo = "";
-        $this->endereco = "";
-        $this->corPrincipal = "";
-        $this->cnpj = "";
-        $this->inscricaoEstadual = "";
-        $this->taxaDesconto = "";
-    }
-
-    function getNomeLoja(){
-        return $this->nomeLoja;
-    }
-
-    function getLogo(){
-        return $this->logo;
-    }
-
-    function getEndereco(){
-        return $this->endereco;
-    }
-
-    function getCorPrincipal(){
-        return $this->corPrincipal;
-    }
-
-    function getCnpj(){
-        return $this->cnpj;
-    }
-    //emitir nota fiscal
-    function getInscricaoEstadual(){
-        return $this->inscricaoEstadual;
-    }
-    //taxa de descinto automatica.
-    function getTaxaDesconto(){
-        return $this->taxaDesconto;
-    }
-    // 
-}
-
-=======
 <?php
 
 require_once("Gerente.php");
@@ -91,5 +36,4 @@ class Config{
         return $this->taxaDesconto;
     }
 }
->>>>>>> upstremb/master:CONTROLLER/Classes/Configuracao.php
 ?>
--- CONTROLLER/Classes/Funcionario.php
+++ CONTROLLER/Classes/Funcionario.php
@@ -1,72 +1,3 @@
-<<<<<<< HEAD:CONTROLLER/Classes/Funcionario.php
-<?php
-
-require("User.php");
-require("Caixa.php");
-require("Movimentacao");
-require("Cliente.php");
-
-class Funcionario {
-    private $nomeCompleto;
-    private $dataNascimento;
-    private $email;
-    private $telefone;
-    private $cpf;
-    private $endereco;
-    private $foto;
-
-    function Funcionario($userCod_user){
-        $this->nomeCompleto = "";
-        $this->dataNascimento = "";
-        $this->email = "";
-        $this->telefone = "";
-        $this->cpf = "";
-        $this->endereco = "";
-        $this->foto = "";
-    }
-
-    function getNomeCompleto(){
-        return $this->nomeCompleto;
-    }
-
-    function getDataNascimento(){
-        return $this->dataNascimento;
-    }
-
-    function getEmail(){
-        return $this->email;
-    }
-
-    function getTelefone(){
-        return $this->telefone;
-    }
-
-    function getCPF(){
-        return $this->cpf;
-    }
-
-    function getEndereco(){
-        return $this->endereco;
-    }
-
-    function getFoto(){
-        return $this->foto;
-    }
-
-    function RegistrarCliente(){
-        //cadastro
-        
-    }
-
-    function RealizarVenda(){
-        
-    }
-
-    function VerCaixa(){
-        
-    }
-}
-=======
 <?php
 
 class Funcionario {
@@ -110,5 +41,4 @@ class Funcionario {
         return $this->endereco;
     }
 }
->>>>>>> upstremb/master:CONTROLLER/Classes/Funcionario.php
 ?>
