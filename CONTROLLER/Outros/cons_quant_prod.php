96ab6180a5ab5b8310594e9d4f7ee607d1dae886
Herminho <43438821+Hermany13@users.noreply.github.com>
2019-06-07 11:19:46 -0300

    Add files via upload

--- CONTROLLER/Outros/cons_quant_prod.php
+++ CONTROLLER/Outros/cons_quant_prod.php
@@ -1,12 +1,31 @@
 <?php
 
-function impListaProdutos($caminho){
+function impListaProdutos($caminho,$caminho2){
 
     require_once($caminho);
-
     $prod = new DAOProdutos();
+    $result = $prod->readsProdutos();
 
-    $prod->readsProdutos();
+    require_once($caminho2);
+    $moviG= new DAOMovimento_Geral();
 
+    while ($i = $result->fetch_assoc()) {
+
+        echo "<tr>";
+
+        echo "<td>" .$i["cod_prod"]. "</td>";
+        echo "<td>" .$i["nome"]. "</td>";
+        echo "<td>" .$i["valor_venda"]. "</td>";
+
+        echo "<td>".$moviG->countItems($i['cod_prod'])."</td>";
+
+        $dados='"listProd",'.'"'.$i["cod_prod"].'","'.$i["nome"].'","'.$i["valor_venda"].'"';
+
+        echo "<td class='actions'>
+             <button class='btn btn-success btn-xs' onclick='addList($dados);'>Adicionar</button>
+             </td>";
+
+        echo "</tr>";
+    }
 }
 ?>
